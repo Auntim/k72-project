801ae42f30c32de9e9cc71ec1c5a4597ec0d7161
auntim <hossennull@gmail.com>
2025-10-17 01:20:30 +0600

    add project and agence secitom UI

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -9,12 +9,12 @@ const HomeBottomText = () => {
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; K72 is an agency that builds brands from every angle. Today, tomorrow and years from now. We think the best sparks fly when comfort zones get left behind and friction infuses our strategies, brands and communications with real feeling. We’re transparent, honest and say what we mean, and when we believe in something, we’re all in.</p>
 
 
-      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase  px-8 py-2'>
+      <div className='lg:border-3 border-2 hover:bg-gray-900 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center border-white rounded-full uppercase  px-8 py-2'>
         <Link className='text-[4vw] lg:mt-4' to='/projects'>Projects</Link>
       </div>
 
 
-      <div className='lg:border-3 border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center px-8 py-2 border-white rounded-full uppercase'>
+      <div className='lg:border-3 border-2 hover:bg-gray-900 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center px-8 py-2 border-white rounded-full uppercase'>
         <Link className='text-[4vw] lg:mt-4' to='/agence'>agence</Link>
       </div>
     </div>
--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -11,22 +11,22 @@ const Agence = () => {
   const imageRef = useRef(null)
 
   const imageArray = [
-    'https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/Olivier_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/Lawrence_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/HugoJoseph_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/ChantalG_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
-    'https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg',
+    'https://i.ibb.co.com/S4wr06jH/profile-1.jpg',
+    'https://i.ibb.co.com/tQjsrC3/1000017694.jpg',
+    'https://i.ibb.co.com/hd4m7Yp/carousel1.jpg',
+    'https://i.ibb.co.com/rmMy6Pg/Screenshot-21.png',
+    'https://i.ibb.co.com/T8jJ6S8/Screenshot-24.png',
+    'https://i.ibb.co.com/B6HgJjz/Screenshot-17.png',
+    //   'https://k72.ca/uploads/teamMembers/SophieA_480x640-480x640.jpg',
+    //   'https://k72.ca/uploads/teamMembers/Claire_480x640-480x640.jpg',
+    //   'https://k72.ca/uploads/teamMembers/Michele_480X640-480x640.jpg',
+    //   'https://k72.ca/uploads/teamMembers/MEL_480X640-480x640.jpg',
+    //   'https://k72.ca/uploads/teamMembers/CAMILLE_480X640_2-480x640.jpg',
+    //   'https://k72.ca/uploads/teamMembers/MAXIME_480X640_2-480x640.jpg',
+    //   'https://k72.ca/uploads/teamMembers/MEGGIE_480X640_2-480x640.jpg',
+    //   'https://k72.ca/uploads/teamMembers/joel_480X640_3-480x640.jpg',
+    // ]
   ]
-
   useGSAP(function () {
 
     gsap.to(imageDivRef.current, {
@@ -60,7 +60,7 @@ const Agence = () => {
     <div className='parent'>
       <div id='page1' className='py-1 '>
         <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[20vw] h-[30vw] lg:rounded-3xl rounded-xl lg:w-[15vw] w-[25vw] lg:top-40 -top-20 lg:left-[30vw] left-[30vw]'>
-          <img ref={imageRef} className='h-full object-cover w-full' src="https://k72.ca/uploads/teamMembers/Carl_480x640-480x640.jpg" alt="" />
+          <img ref={imageRef} className='h-full object-cover w-full' src="https://i.ibb.co.com/B6HgJjz/Screenshot-17.png" alt="" />
         </div>
         <div className='relative font-[font2]'>
           <div className='lg:mt-[55vh] mt-[30vh]'>
